76e4c9534f4b84ef772c1cc3584ce7f30bba8ff0
jthomas47 <jackspurthomas@gmail.com>
2023-11-14 08:42:36 -0600

    created function to display forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,57 @@
 var searchBtn = document.querySelector('#searchBtn');
 var dailyWeather = document.querySelector('#dailyWeather'); 
+var forecast = document.querySelector('#forecast'); 
+var APIKey = '7179e619355c98dfb6b8db33795ab22d';
 
 function convertDate(dt) {
     console.log(dt); 
     var date = new Date(dt * 1000); 
     return date.toLocaleDateString("en-US"); 
+}
+
+function getForecast(lat, lon) {
+console.log(lat); 
+console.log(lon);
+var units = 'imperial';
+var lang = 'en'; 
+var queryUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}&units=${units}&lang=${lang}`;
+fetch(queryUrl)
+        .then(function (response) {
+            if (!response.ok) {
+                throw response.json();
+            }
+            return response.json();
+        })
+        .then(function (data) {
+            showForecast(data); 
+        })
+        .catch(console.err);
+
+}
+
+function showForecast(resp) {
+    console.log(resp); 
+    forecast.innerHTML = resp.list.map((day,idx) => {
+        if (idx >= 8 && idx % 8 == 0) {
+            console.log(convertDate(day.dt));
+            
+           return `<div class="col">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">${convertDate(day.dt)} </h5>
+                <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}.png"/>
+                <p class="card-text">Temp: ${day.main.temp}&deg;F</p>
+                <p class="card-text">Wind: ${day.wind.speed} MPH</p>
+                <p class="card-text">Humidity: ${day.main.humidity} %</p>
+              </div>
+            </div>
+          </div>`
+           
+        }
+    }).join(' ')
+
+    console.log(forecast);
+    
 }
 
 function showWeather(resp) {
@@ -15,6 +62,9 @@ function showWeather(resp) {
     var wind = resp.wind.speed; 
     var humidity = resp.main.humidity; 
     var today = convertDate(resp.dt); 
+    var lat = resp.coord.lat;
+    var lon = resp.coord.lon;
+    getForecast(lat, lon); 
     var weatherHtml = `<h3>${city} (${today}) <img src="http://openweathermap.org/img/wn/${icon}.png"/></h3>
   
     <p>Temp: ${temp}&deg;F</p>
@@ -26,7 +76,6 @@ function showWeather(resp) {
 }
 
 function getWeather() {
-    var APIKey = '7179e619355c98dfb6b8db33795ab22d';
     var city = 'Wichita';
     var units = 'imperial';
     var lang = 'en'; 
