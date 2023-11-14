ebde152be1972e012785ac31eefcfcf9b66f6be7
jthomas47 <jackspurthomas@gmail.com>
2023-11-14 06:07:49 -0600

    created function to generate html and convert unix time stamp

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,33 @@
 var searchBtn = document.querySelector('#searchBtn');
+var dailyWeather = document.querySelector('#dailyWeather'); 
 
+function convertDate(dt) {
+    console.log(dt); 
+    var date = new Date(dt * 1000); 
+    return date.toLocaleDateString("en-US"); 
+}
 
 function showWeather(resp) {
-    console.log(resp); 
+    console.log(resp);
+    var city = resp.name
+    var temp = resp.main.temp; 
+    var wind = resp.wind.speed; 
+    var humidity = resp.main.humidity; 
+    var today = convertDate(resp.dt); 
+    var weatherHtml = `   <h3>${city} (${today})</h3>
+    <p>Temp: ${temp}&deg;F</p>
+    <p>Wind: ${wind} MPH</p>
+    <p>Humidity: ${humidity} %</p>`;
+    console.log(weatherHtml); 
+
 }
 
 function getWeather() {
     var APIKey = '7179e619355c98dfb6b8db33795ab22d';
-    var city = 'Wichita'
-    var queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    var city = 'Wichita';
+    var units = 'imperial';
+    var lang = 'en'; 
+    var queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=" + units + "&lang=" + lang;
     fetch(queryUrl)
         .then(function (response) {
             if (!response.ok) {
